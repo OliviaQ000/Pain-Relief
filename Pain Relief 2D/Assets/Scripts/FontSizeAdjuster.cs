ea95619a05f5701dbe9d3de2c10de407ecb32e93
OliviaQ000 <heixingy@gmail.com>
2025-11-13 17:02:17 +0000

    13112025update

---
+++ Pain Relief 2D/Assets/Scripts/FontSizeAdjuster.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FontSizeAdjuster : MonoBehaviour
+{
+    public Scrollbar fontSizeScrollbar; // Connect in inspector
+    public TMP_Text dialogueText;
+    public float minFontSize = 16f;
+    public float maxFontSize = 40f;
+
+    void Start()
+    {
+        // Check change of scrollbar
+        fontSizeScrollbar.onValueChanged.AddListener(OnFontSizeChanged);
+
+        OnFontSizeChanged(fontSizeScrollbar.value); //Set to initial
+    }
+
+    void OnFontSizeChanged(float value)
+    {
+        dialogueText.fontSize = Mathf.Lerp(minFontSize, maxFontSize, value);
+    }
+}
